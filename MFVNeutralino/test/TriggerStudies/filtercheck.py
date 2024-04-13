3dad7bb9cffe22d6514a992ab06cf662386972f6
Abigail Warden <acwarden@wisc.edu>
2024-04-13 10:18:47 -0500

    small trigger cleanup

--- MFVNeutralino/test/TriggerStudies/filtercheck.py
+++ MFVNeutralino/test/TriggerStudies/filtercheck.py
@@ -1,13 +1,18 @@
 import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import *
+from JMTucker.MFVNeutralino.NtupleCommon import signal_uses_random_pars_modifier
 
 settings = CMSSWSettings()
 settings.is_mc = True
 settings.is_miniaod = True
 settings.cross = '' # 2017to2018' # 2017to2017p8'
 
-#sample_files(process, 'qcdht1000_2017', 'miniaod')
-sample_files(process, 'mfv_stoplb_tau000100um_M0800_2017', 'miniaod')
+#for submission to condor
+randpars_filter = False
+#for testing local :
+#randpars_filter = 'randpar HToSSTobbbb M15_ct10-'
+
+sample_files(process, 'qcdht1000_2017', 'miniaod')
 geometry_etc(process, settings)
 tfileservice(process, 'filtercheck.root')
 cmssw_from_argv(process)
@@ -15,16 +20,15 @@ cmssw_from_argv(process)
 from JMTucker.MFVNeutralino.EventFilter import setup_event_filter
 sef = lambda *a,**kwa: setup_event_filter(process, *a, input_is_miniaod=True, **kwa)
 sef('pTrigger', mode = 'trigger jets only')
-#sef('pTriggerMET', mode = 'trigger met only', name_ex = 'met')
-#sef('pTriggerBjets', mode = 'trigger bjets only',name_ex = 'bjets')
-#sef('pTriggerDispDijet', mode = 'trigger displaced dijet only',name_ex = 'displaced_dijet')
-#sef('pTriggerOR', mode = 'trigger HT OR bjets OR displaced dijet', name_ex = 'HT_OR_bjets_OR_displaced_dijet')
-#sef('pJets',    mode = 'jets only novtx',   name_ex = 'NoVtx') # be sure to generate a different name for each subsequent use
-#sef('pNoJESUp', mode = 'jets only novtx',   name_ex = 'NoJESUp', event_filter_jes_mult = 0)
-#sef('pFull',    mode = 'jets only',         name_ex = 'Full') # uncomment to get efficiency of ntuple-level vertex filter
+sef('pTriggerMET', mode = 'trigger met only', name_ex = 'met')
+sef('pTriggerBjets', mode = 'trigger bjets only',name_ex = 'bjets')
+sef('pTriggerDispDijet', mode = 'trigger displaced dijet only',name_ex = 'displaced_dijet')
+sef('pTriggerOR', mode = 'trigger HT OR bjets OR displaced dijet', name_ex = 'HT_OR_bjets_OR_displaced_dijet')
 sef('pTriggerLeptons', mode = 'trigger leptons only',name_ex = 'leptons')
-
-
+sef('pTriggerLeptonsORDispLep', mode = 'trigger leptons OR displaced leptons',name_ex = 'leptons_OR_displep')
+sef('pJets',    mode = 'jets only novtx',   name_ex = 'NoVtx') # be sure to generate a different name for each subsequent use
+sef('pNoJESUp', mode = 'jets only novtx',   name_ex = 'NoJESUp', event_filter_jes_mult = 0)
+#sef('pFull',    mode = 'jets only',         name_ex = 'Full') # uncomment to get efficiency of ntuple-level vertex filter
 
 if len(process.mfvTriggerFilter.HLTPaths) > 1:
     for x in process.mfvTriggerFilter.HLTPaths:
@@ -47,7 +51,8 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
         #samples = Samples.all_signal_samples_2017
         #samples += Samples.leptonic_samples_2017
     elif year == 2018:
-        samples = Samples.qcd_samples_2018 + Samples.data_samples_2018
+        #samples = Samples.qcd_samples_2018 + Samples.data_samples_2018
+        samples = Samples.mfv_stoplb_samples_2018 + Samples.mfv_stopld_samples_2018
         #samples = Samples.all_signal_samples_2018
 
     ms = MetaSubmitter('TrigFiltCheckV3', dataset='miniaod')
