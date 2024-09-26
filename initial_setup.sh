1e4e3180e345be1750919ab7cf201d2715971d7c
Joseph Reichert <Joseph.Reichert@cern.ch>
2024-09-26 14:25:37 -0400

    fix for ssh keys in initial setup script

--- initial_setup.sh
+++ initial_setup.sh
@@ -3,7 +3,8 @@ scram project -n mfv_10627 CMSSW CMSSW_10_6_27
 cd mfv_10627/src
 cmsenv
 git cms-init --upstream-only
-git clone https://github.com/DisplacedVertices/cmssw-usercode.git JMTucker
+#git clone https://github.com/DisplacedVertices/cmssw-usercode.git JMTucker # we can pull via https but not push. Leaving this here in case someone doesn't have ssh keys set up...
+git clone git@github.com:DisplacedVertices/cmssw-usercode.git
 cd JMTucker
 scram b -j 4
 statuscode=$?
